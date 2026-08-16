c7ee4b39d6eb5565f6e77cd69fc3281843b50cb8
Srinidhi Lokesh <srinidhilokesh55@outlook.com>
2026-08-16 02:09:52 -0700

    Make Lenis scroll effect more pronounced
    Switch from continuous lerp smoothing (subtle, easy to miss on
    trackpads) to duration+easing tween mode, which animates each
    scroll into a clear glide. Also sync touch input so trackpad
    gestures get smoothed too.

--- app/components/SmoothScroll.tsx
+++ app/components/SmoothScroll.tsx
@@ -7,6 +7,9 @@ export function SmoothScroll() {
   useEffect(() => {
     const lenis = new Lenis({
       autoRaf: true,
+      duration: 1.2,
+      easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      syncTouch: true,
     });
 
     return () => {
